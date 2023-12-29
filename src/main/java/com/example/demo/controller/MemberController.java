aa35499d098092d8047c709305c1e54c9889c69b
zkti1 <zkti12345@naver.com>
2023-12-29 11:40:46 +0900

    join-the-membership

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,5 +13,15 @@ public class MemberController {
     @GetMapping("/member/save")
     public String saveForm() {
         return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword")String memberPassword,
+                       @RequestParam("meberName")String memberName) {
+
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+        return "index";
     }
 }
